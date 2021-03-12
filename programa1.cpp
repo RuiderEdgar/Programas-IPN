268bd9bbcee956c2ed3a71761c522b0cf8859ec0
RuiderEdgar <75879207+RuiderEdgar@users.noreply.github.com>
2021-03-12 04:44:10 -0600

    Add files via upload
    POO - 1er ejercicio

---
+++ programa1.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string.h>
+
+
+using namespace std;
+
+
+class Diaanio{
+    public:
+        void visualizar();
+        int mes;
+        int dia;
+};
+
+void Diaanio::visualizar(){
+    char nommes[80];
+    
+    if(mes == 01) strcpy(nommes, "Enero");
+    if(mes == 02) strcpy(nommes, "Febrero");
+    if(mes == 03) strcpy(nommes, "Marzo");
+    if(mes == 04) strcpy(nommes, "Abril");
+    if(mes == 05) strcpy(nommes, "Mayo");
+    if(mes == 06) strcpy(nommes, "Junio");
+    if(mes == 07) strcpy(nommes, "Julio");
+    if(mes == 8) strcpy(nommes, "Agosto");
+    if(mes == 9) strcpy(nommes, "Septiembre");
+    if(mes == 10) strcpy(nommes, "Octubre");
+    if(mes == 11) strcpy(nommes, "Noviembre");
+    if(mes == 12) strcpy(nommes, "Diciembre");
+    if(mes == 00 && mes > 12) strcpy(nommes, "Mes no valido");
+    cout<< dia <<":"<<nommes<<"\n";
+}
+
+int main(){
+    
+
+    Diaanio hoy, cumpleanios;
+    cout<<"\tIntrodusca la fecha del dia de hoy \n\n";
+    cout<<"Introdusca el numero del mes: ";
+    cin>>hoy.mes;
+    cout<<"\nIntrodusca el numero del dia: ";
+    cin>>hoy.dia;
+    cout<<"\n";
+    cout<<"\tIntrodusca su fecha de nacimiento \n\n";
+    cout<<"Introdusca el numero del mes: ";
+    cin>>cumpleanios.mes;
+    cout<<"\nIntrodusca el numero del dia: ";
+    cin>>cumpleanios.dia;
+    cout<<"\n\nLa fecha del dia de hoy es: ";
+    hoy.visualizar();
+     cout<<"\nSu fecha del dia de hoy es: ";
+    cumpleanios.visualizar();
+
+    if(hoy.mes == cumpleanios.mes && hoy.dia == cumpleanios.dia){
+        cout<<"\n\t FELIZ CUMPLEAÑOS! \n";
+    }
+    else{
+        cout<<"\n\t Hoy no es tu cumpleaños :c\n";
+    }
+    system("pause");
+}
